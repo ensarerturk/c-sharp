ec041c3af347886ccdd943d8817a62162361b860
ensarerturk <ensarerturk15@gmail.com>
2023-11-22 22:33:38 +0300

    first commit

---
+++ Constructors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee firstEmp = new Employee("Ensar", "Erturk", 123123, "Computer");
+            firstEmp.EmployeeInfo();
+            Console.WriteLine("*********************************");
+
+            Employee secondEmp = new Employee("Furkan", "Yildiz", 321321, "IK");
+            secondEmp.EmployeeInfo();
+        }
+    }
+
+    class Employee
+    {
+        public string name;
+        public string surname;
+        public int no;
+        public string department;
+
+        public Employee(string name, string surname, int no, string department) {
+            this.name = name;
+            this.surname = surname;
+            this.no = no;
+            this.department = department;
+        }
+
+        public void EmployeeInfo() 
+        {
+            Console.WriteLine("Employee Name: {0}", name);
+            Console.WriteLine("Employee Surname: {0}", surname);
+            Console.WriteLine("Employee's No: {0}", no);
+            Console.WriteLine("Employee Department: {0}", department);
+        }
+    }
+}
---
+++ Instance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee firstEmp = new Employee();
+            firstEmp.name = "Ensar";
+            firstEmp.surname = "Erturk";
+            firstEmp.no = 123123;
+            firstEmp.department = "Computer";
+            firstEmp.EmployeeInfo();
+            Console.WriteLine("*********************************");
+
+            Employee secondEmp = new Employee();
+            secondEmp.name = "Furkan";
+            secondEmp.surname = "Yildiz";
+            secondEmp.no = 321321;
+            secondEmp.department = "IK";
+            secondEmp.EmployeeInfo();
+        }
+    }
+
+    class Employee
+    {
+        public string name;
+        public string surname;
+        public int no;
+        public string department;
+        
+        public void EmployeeInfo() 
+        {
+            Console.WriteLine("Employee Name: {0}", name);
+            Console.WriteLine("Employee Surname: {0}", surname);
+            Console.WriteLine("Employee's No: {0}", no);
+            Console.WriteLine("Employee Department: {0}", department);
+        }
+    }
+}
+
+
